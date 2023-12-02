d57700047ce99d8658482e597bdad8d7b87ec15a
20017651 <152613045+20017651@users.noreply.github.com>
2023-12-02 23:13:52 +0000

    app

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from logging.config import dictConfig
 dictConfig({
     'version': 1,
     'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s', 
     }},
     'handlers': {'wsgi': {
         'class': 'logging.StreamHandler',
